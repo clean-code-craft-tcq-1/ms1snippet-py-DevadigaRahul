aecc4fae4cce9c5cfd13b9b7ea2566c179f3f2dc
Rahul Devadiga <Devadiga.Rahul@in.bosch.com>
2021-03-25 13:11:47 +0530

    commit

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,16 +1,18 @@
-'''def Is_not_empty_list(values):
-  return (isinstance(values, list) and (len(values)!=0))'''
+def Is_not_empty_list(values):
+  return (isinstance(values, list) and (len(values)!=0))
 
 def  Consecutive_Values_Difference_is_less_Than_maxDelta(value, nextValue, maxDelta):
   return(nextValue - value < maxDelta)
 
-def reports_sudden_jump_in_reading(values,maxDelta):
-  #if(Is_not_empty_list(values)):
-  if(values!=None):
+def test_sudden_jump_in_reading(values,maxDelta):
     last_but_one_reading = len(values) - 1
     for i in range(last_but_one_reading):
       if(not Consecutive_Values_Difference_is_less_Than_maxDelta(values[i], values[i + 1], maxDelta)):
         return False
     return True
+  
+def reports_sudden_jump_in_reading(values,maxDelta):
+  if(Is_not_empty_list(values)):
+    return (test_sudden_jump_in_reading(values,maxDelta))
 
 
